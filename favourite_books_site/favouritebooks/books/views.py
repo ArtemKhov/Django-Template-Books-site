8da9516a0d324d6001bf1804875c356f13602968
Artem <artem.kotyakhov@gmail.com>
2024-10-03 23:48:46 +0300

    add header

--- favourite_books_site/favouritebooks/books/views.py
+++ favourite_books_site/favouritebooks/books/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+navbar = [{'title': "About", 'url_name': 'about'},
+        {'title': "Add Post", 'url_name': 'add_post'},
+        {'title': "Feedback", 'url_name': 'feedback'},
+]
+
 def index(request):
-    data = {'title': 'Favourite Books'}
-    return render(request, 'books/index.html', data)
+    data = {
+        'title': 'Favourite Books',
+        'navbar': navbar,
+    }
+    return render(request, 'books/index.html', context=data)
+
+def about(request):
+    data = {
+        'title': 'About',
+        'navbar': navbar,
+    }
+    return render(request, 'books/about.html', context=data)
+
+def add_post(request):
+    data = {
+        'title': 'Add Post',
+        'navbar': navbar,
+    }
+    return render(request, 'books/add_post.html', context=data)
+
+def feedback(request):
+    data = {
+        'title': 'Feedback',
+        'navbar': navbar,
+    }
+    return render(request, 'books/feedback.html', context=data)
+
+
 
 
