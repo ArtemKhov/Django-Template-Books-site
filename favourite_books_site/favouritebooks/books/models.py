2271308d6006418fa4737de9f8cb1a24904c8788
Artem <artem.kotyakhov@gmail.com>
2024-10-06 20:53:52 +0300

    add Book model and default  models

--- favourite_books_site/favouritebooks/books/models.py
+++ favourite_books_site/favouritebooks/books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    '''
+    Book info
+    '''
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+
