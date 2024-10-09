ee37ed62a7cdd732575937148107e7ac10c6ab20
Artem <artem.kotyakhov@gmail.com>
2024-10-09 20:45:25 +0300

    set status to PUBLISHED as default

--- favourite_books_site/favouritebooks/books/models.py
+++ favourite_books_site/favouritebooks/books/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_published=Book.Status.PUBLISHED)
+
 
 class Book(models.Model):
     '''
@@ -18,7 +20,7 @@ class Book(models.Model):
     description = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.PUBLISHED)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
     genre = models.ForeignKey('Genres', on_delete=models.PROTECT)
 
@@ -36,6 +38,10 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse('book', kwargs={'book_slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
 class Genres(models.Model):
     genre = models.CharField(max_length=100, db_index=True)
