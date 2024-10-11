56ef0f68ae09be04c9b13594729c42f20350dfc8
Artem <artem.kotyakhov@gmail.com>
2024-10-11 21:11:25 +0300

    show only those tags that have one or more books

--- favourite_books_site/favouritebooks/books/templatetags/book_tags.py
+++ favourite_books_site/favouritebooks/books/templatetags/book_tags.py
@@ -1,8 +1,13 @@
 from django import template
+from django.db.models import Count
+
 from books.models import Genres
 
 register = template.Library()
 
 @register.inclusion_tag('books/list_tags.html')
 def show_all_tags():
-    return {'tags': Genres.objects.all(), }
+    '''
+    Select only those tags that are related to at least one book
+    '''
+    return {'tags': Genres.objects.annotate(total=Count('genres')).filter(total__gt=0)}
