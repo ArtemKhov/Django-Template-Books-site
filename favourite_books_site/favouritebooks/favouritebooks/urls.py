dd12aeb42538748b0522aa3d8fcdf140e25c7aa1
Artem <artem.kotyakhov@gmail.com>
2024-10-18 21:13:30 +0300

    add static root

--- favourite_books_site/favouritebooks/favouritebooks/urls.py
+++ favourite_books_site/favouritebooks/favouritebooks/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 from favouritebooks import settings
 
 urlpatterns = [
@@ -13,3 +12,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
