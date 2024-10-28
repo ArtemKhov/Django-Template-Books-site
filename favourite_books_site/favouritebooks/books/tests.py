40a2dffb5529e47eb9413c8e712f9410a43af3d7
Artem <artem.kotyakhov@gmail.com>
2024-10-28 21:13:11 +0300

    add some tests

--- favourite_books_site/favouritebooks/books/tests.py
+++ favourite_books_site/favouritebooks/books/tests.py
@@ -1,11 +1,93 @@
+from http import HTTPStatus
+
 from django.test import TestCase
+from django.urls import reverse
+
+from books.models import Book
+
 
 class GetPagesTestCase(TestCase):
+
+    def check_correct_navbar(self, response):
+        navbar_data = response.context_data['navbar']
+        navbar_titles = ['Home', 'All books', 'My books', 'Add Book', 'Feedback']
+        navbar_urls = ['home', 'books', 'user_books', 'add_book', 'feedback']
+
+        self.assertEqual(len(navbar_data), 5)
+
+        for item in navbar_data:
+            self.assertIn(item['title'], navbar_titles)
+            self.assertIn(item['url_name'], navbar_urls)
+
+
     def setUp(self):
         "init"
 
-    def test_case_1(self):
-        pass
+
+    def test_main_page(self):
+        '''
+        Check if the main page is displayed correctly when a request is made
+        '''
+
+        path = reverse('home')
+        response = self.client.get(path)
+
+        # check: get main page - status 200
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        # check: get correct template
+        self.assertTemplateUsed(response, 'books/index.html')
+
+        # check: correct template in template_name
+        self.assertIn('books/index.html', response.template_name)
+
+        # check: correct title
+        self.assertEqual(response.context_data['title'], 'Favourite Books')
+
+        # check: correct navbar data
+        self.check_correct_navbar(response)
+
+
+    def test_redirect_addpage(self):
+        '''
+        Check Add Book page working correctly
+        '''
+
+        path = reverse('add_book')
+
+        redirect_url = reverse('users:login') + '?next=' + path
+        response = self.client.get(path)
+
+        # check: redirect work correctly
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+        # check: redirect to correct page
+        self.assertRedirects(response, redirect_url)
+
+
+    def test_data_all_books(self):
+        '''
+        Check if the All Books page is displayed correctly when a request is made
+        '''
+
+        path = reverse('books')
+        response = self.client.get(path)
+
+        # check: get page - status 200
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        # check: get correct template
+        self.assertTemplateUsed(response, 'books/books.html')
+
+        # check: correct template in template_name
+        self.assertIn('books/books.html', response.template_name)
+
+        # check: correct title
+        self.assertEqual(response.context_data['title'], 'All Books')
+
+        # check: correct navbar data
+        self.check_correct_navbar(response)
+
 
     def tearDown(self):
         "clean"
--- favourite_books_site/favouritebooks/users/tests.py
+++ favourite_books_site/favouritebooks/users/tests.py
@@ -1,3 +1,88 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RegisterUserTestCase(TestCase):
+
+    def setUp(self):
+        self.data = {
+            'username': 'user_test',
+            'email': 'usertest@mail.ru',
+            'password1': '12345678Aa',
+            'password2': '12345678Aa',
+        }
+
+
+    def test_form_registration_get(self):
+        '''
+        Check if the registration form is displayed correctly when a request is made
+        '''
+
+        path = reverse('users:register')
+        response = self.client.get(path)
+
+        # check: get page - status 200
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        # check: get correct template
+        self.assertTemplateUsed(response, 'users/register.html')
+
+
+    def test_user_registration_success(self):
+        '''
+        Checking the correctness of user registration
+        '''
+
+        user_model = get_user_model()
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+
+        # check: redirect after success registration
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+
+        # check: redirect to register-success page
+        self.assertRedirects(response, reverse('users:register_success'))
+
+        # check: new user add to DB
+        self.assertTrue(user_model.objects.filter(username=self.data['username']).exists())
+
+
+    def test_user_registration_password_error(self):
+        '''
+        Checking that the correct error appears when the wrong password is entered during registration.
+        '''
+
+        self.data['password2'] = '12345678A'
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+
+        # check: code 200 even if put incorrect data
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        # check: valid error message
+        self.assertContains(response, "The two password fields didn’t match.", html=True)
+
+
+    def test_user_registration_user_exists_error(self):
+        '''
+        Checking that users with the same login cannot be registered.
+        '''
+
+        user_model = get_user_model()
+        user_model.objects.create(username=self.data['username'])
+
+        path = reverse('users:register')
+        response = self.client.post(path, self.data)
+
+        # check: code 200 even if put incorrect data
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        # check: user already exists error message
+        self.assertContains(response, "A user with that username already exists.")
+
+
+    def tearDown(self):
+        "clean"
